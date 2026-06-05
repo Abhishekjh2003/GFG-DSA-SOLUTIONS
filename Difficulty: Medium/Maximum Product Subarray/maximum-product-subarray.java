0496a58e1364b827686b07edf805b38470593059
ABHISHEK HUDED <abhishekhuded506@gmail.com>
2026-06-05 22:24:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.java
+++ Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.java
@@ -1,18 +1,25 @@
 class Solution {
     int maxProduct(int[] arr) {
         // code here
-        int minprod=arr[0];
-        int maxprod=arr[0];
-        int result =arr[0];
-        for(int i =1;i<arr.length;i++)
-        {
-            int tempmax=Math.max(arr[i],Math.max(arr[i]*minprod,arr[i]*maxprod));
-            int tempmin=Math.min(arr[i],Math.min(arr[i]*minprod,arr[i]*maxprod));
-           minprod=tempmin;
-           maxprod=tempmax; 
-           result =Math.max(result,maxprod);
+        int maxProd = arr[0];
+        int minProd = arr[0];
+        int ans = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            int num = arr[i];
+
+            if (num < 0) {
+                int temp = maxProd;
+                maxProd = minProd;
+                minProd = temp;
+            }
+
+            maxProd = Math.max(num, maxProd * num);
+            minProd = Math.min(num, minProd * num);
+
+            ans = Math.max(ans, maxProd);
         }
-        
-        return result;
+
+        return ans;
     }
 }
