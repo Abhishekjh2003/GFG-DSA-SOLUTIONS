4e7a0420462e5d0ef5360832a8011e386644fe21
ABHISHEK HUDED <abhishekhuded506@gmail.com>
2026-07-06 19:18:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.java
+++ Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.java
@@ -1,21 +1,24 @@
+import java.util.*;
+
 class Solution {
     public int cntSubarrays(int[] arr, int k) {
-        // code here
-        HashMap<Integer,Integer> m =new HashMap<>();
-        m.put(0,1);
-        int sum =0;
-        int count =0;
-        for(int num:arr)
-        {
-            sum+=num;
-            
-            
-            if(m.containsKey(sum-k))
-            {
-                count+=m.get(sum-k);
-            }
-            m.put(sum,m.getOrDefault(sum,0)+1);
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        int prefixSum = 0;
+        int count = 0;
+
+        for (int num : arr) {
+
+            prefixSum += num;
+
+            count += map.getOrDefault(prefixSum - k, 0);
+
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
         }
+
         return count;
     }
 }
