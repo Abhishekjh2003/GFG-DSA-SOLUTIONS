8c05fb93af892598dc14ed64efe3f2fe3f79bf35
ABHISHEK HUDED <abhishekhuded506@gmail.com>
2026-07-17 18:58:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Occurences of Anagrams/count-occurences-of-anagrams.java
+++ Difficulty: Medium/Count Occurences of Anagrams/count-occurences-of-anagrams.java
@@ -1,43 +1,40 @@
-// User function Template for Java
-
 class Solution {
-
     int search(String pat, String txt) {
-        // code here
         int k = pat.length();
         int n = txt.length();
 
-        int[] pCount = new int[26];
-        int[] window = new int[26];
+        int[] patFreq = new int[26];
+        int[] winFreq = new int[26];
 
-        for (int i = 0; i < k; i++) {
-            pCount[pat.charAt(i) - 'a']++;
-            window[txt.charAt(i) - 'a']++;
+        for (char ch : pat.toCharArray()) {
+            patFreq[ch - 'a']++;
         }
 
         int count = 0;
 
-        if (isSame(pCount, window)) {
-            count++;
-        }
+        for (int i = 0; i < n; i++) {
+            winFreq[txt.charAt(i) - 'a']++;
 
-        for (int i = k; i < n; i++) {
-            window[txt.charAt(i) - 'a']++;
-            window[txt.charAt(i - k) - 'a']--;
+            if (i >= k) {
+                winFreq[txt.charAt(i - k) - 'a']--;
+            }
 
-            if (isSame(pCount, window)) {
-                count++;
+            if (i >= k - 1) {
+                boolean same = true;
+
+                for (int j = 0; j < 26; j++) {
+                    if (patFreq[j] != winFreq[j]) {
+                        same = false;
+                        break;
+                    }
+                }
+
+                if (same) {
+                    count++;
+                }
             }
         }
 
         return count;
-    }
-
-    private boolean isSame(int[] a, int[] b) {
-        for (int i = 0; i < 26; i++) {
-            if (a[i] != b[i]) return false;
-        }
-        return true;
-        
     }
 }
